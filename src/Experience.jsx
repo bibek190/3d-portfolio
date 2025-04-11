ab09a51f93e21d1bd2b7dfffe8f13030afb9518c
bibek190 <bibek9818917036@gmail.com>
2025-04-11 13:12:18 +1000

    first commit

---
+++ src/Experience.jsx
@@ -0,0 +1,75 @@
+import {
+  ContactShadows,
+  Environment,
+  Float,
+  Html,
+  OrbitControls,
+  PresentationControls,
+  Text,
+  useGLTF,
+} from "@react-three/drei";
+import { useControls } from "leva";
+import * as THREE from "three";
+
+export default function Experience() {
+  const model = useGLTF(
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
+  );
+
+  const { float } = useControls("float", {
+    float: { value: 0.4 },
+  });
+  const { color } = useControls("float", {
+    color: { value: "#4e4c29" },
+  });
+
+  console.log(model);
+  return (
+    <>
+      {/* <OrbitControls makeDefault /> */}
+      <Environment files={"./code.jpg"} background />
+      <color args={[color]} attach="background" />
+      <PresentationControls
+        global
+        rotation={[0.13, 0.1, 0]}
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={{ mass: 2, tension: 400 }}
+        snap
+      >
+        <Float rotationIntensity={float}>
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={65}
+            color={"#e3b50c"}
+            rotation={[0.1, Math.PI, 0]}
+            position={[0, 0.55, -1.15]}
+          />
+          <primitive object={model.scene} position-y={-1.2}>
+            <Html
+              transform
+              wrapperClass="htmlScreen"
+              distanceFactor={1.17}
+              position={[0, 1.56, -1.4]}
+              rotation-x={-0.256}
+            >
+              <iframe src="https://www.bibektech.com/" />
+            </Html>
+          </primitive>
+          <Text
+            font="./bangers-v20-latin-regular.woff"
+            fontSize={1}
+            position={[2, 0.75, 0.75]}
+            rotation-y={-1.25}
+            maxWidth={2}
+            color={"#f3de62"}
+          >
+            Bibek SHRESTHA
+          </Text>
+        </Float>
+      </PresentationControls>
+      <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
+    </>
+  );
+}
